c912daa517c279c7d0d9e8483bf450af1bb62b22
Casper Bekmand <casper.bekmand@hotmail.com>
2012-04-19 10:03:23 +0200

    Model: Forum added

---
+++ src/model/forum.java
@@ -0,0 +1,12 @@
+package model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Casper
+ * Date: 19-04-12
+ * Time: 10:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class forum {
+
+}
